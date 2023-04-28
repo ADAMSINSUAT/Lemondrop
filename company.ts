7d9455282588a8f90becee7142ac8b1a17e0ab5f
ADAMSINSUAT <109055704+ADAMSINSUAT@users.noreply.github.com>
2023-04-28 12:57:44 +0800

    Deleted unncessary files

--- company.ts
+++
@@ -1,7 +0,0 @@
-import Company from "../src/modules/company"
-export default [
-    new Company("Lemondrop Technologies, Inc.", 2, 10, "123123ase",
-    ),
-    new Company("Workbean", 2, 20, "123123asd",
-    ),
-]
