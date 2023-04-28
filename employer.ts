291bc0b94a112aa824214ee9c88efdfc5a2f1e65
ADAMSINSUAT <109055704+ADAMSINSUAT@users.noreply.github.com>
2023-04-28 12:25:11 +0800

    Add files via upload

---
+++ employer.ts
@@ -0,0 +1,12 @@
+export default[
+    {
+        "employerID": "1928371892ylajdasljd",
+        "accID": "alsdjkadlj91203",
+        "compID": "123123ase",
+    },
+    {
+        "employerID": "1928371892ylajdaslje",
+        "accID": "alsdjkadlj91204",
+        "compID": "123123asd",
+    }
+]
